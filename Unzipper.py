23e512a5287563199e42f175c5fe27b477c5415b
Mana <88516116+ManaNagase@users.noreply.github.com>
2022-04-20 16:36:38 +0200

    Bro how am i so bad at uploading code
    I uploaded the wrong code

--- Unzipper.py
+++ Unzipper.py
@@ -1,14 +1,33 @@
+#imports
 import zipfile
 import os
 import random
 import string
-def random_char(y):
-    return ''.join(random.choice(string.ascii_letters) for x in range(y))
-xdw = os.listdir("S:\Codingn't\decompiler.py\zip")
+
+#Your Variables
+zip_folder = ""
+output_folder = ""
+
+
+#Function for random folder names
+def random_char(r):
+    return "".join(random.choice(string.ascii_letters) for i in range(r))
+
+#Gets the zip folder
+try:
+    xdw = os.listdir(zip_folder)
+except:
+    print("You shouldn't be seeing this")
+    #Really, If you Do Make A Pull Request Or Set Your Variables In Case You Haven't
+
+
 for i in range(len(xdw)):
     try:
-        with zipfile.ZipFile("S:\Codingn't\decompiler.py\zip\\"+xdw[i],"r") as zip_ref:
-            zip_ref.extractall("S:\RimWorld\Mods\\"+random_char(128))
+        #Unzip
+        #I have no idea how i got this to work, so don't touch anything if you don't know what you're doing
+        with zipfile.ZipFile(zip_folder+"\\"+xdw[i],"r") as zip_ref:
+            zip_ref.extractall(output_folder+"\\"+random_char(128))
         print("Finished")
     except:
-        print("something broke terribly")
+        print("You shouldn't be seeing this")
+        #Really, If you Do Make A Pull Request Or Set Your Variables In Case You Haven't
